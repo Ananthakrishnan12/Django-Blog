3ecfc890bdac13f2cd859ab9ac762462f054c48d
ananthakrishnan <ananthakrishnan@gmail.com>
2025-02-18 12:04:00 +0530

    Blog App & Blog Model

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog_main import settings
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home")
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- notes.md
+++ notes.md
@@ -6,3 +6,7 @@
  Note: gitignore --> Ignoring files that we dont want to have in the Github.
  Go to website.. gitignore.io  type Django and copy all the filenames 
  create .gitignore in the root directory and paste the files.
+
+ Note: slug means url pattern that represent the blog title.It is very useful for SEO.(Search Engine optimization).
+
+ Note: Install Pillow while using Media files.
